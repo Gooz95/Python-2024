691bafb00f45ee4734a2c1c357508f6f42a996c0
siweli <lewisr.py@gmail.com>
2024-05-20 17:03:03 +0100

    lewis

--- siwel_files/siwel.py
+++ siwel_files/siwel.py
@@ -14,7 +14,7 @@ def create_connection():
         con = connect(
             database="gym_db",
             user="postgres",
-            password="axel",
+            password="lewis",
             host="localhost",
             port="5432",
         )
